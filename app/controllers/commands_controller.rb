a3098d697d5dea661dba722e71e8ea234edbe430
Mac Dougherty <mac@macdougherty.com>
2017-12-20 13:27:02 -0500

    reset

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -26,30 +26,27 @@ class CommandsController < ApplicationController
           n.skip_extract = true
           # Setting skip_extract to true skips sending messages to the Apiotics server for delivery to the device.
           # See app/models/neo_pixel.rb for the details of these commands.
-          if false
-            if params[:command] == "on"
-              n.all_on([75,40,3])
-            elsif params[:command] == "off"
-              n.all_off
-            elsif params[:command] == "christmas"
-              n.christmas(10)
-            elsif params[:command] == "big_rainbow"
-              n.big_rainbow
-            elsif params[:command] == "dots"
-              n.moving_dots(10, 4, true, [127,127,0], [0,127,127])
-            elsif params[:command] == "lightshow"
-              n.lightshow
-            elsif params[:command] == "snake"
-              n.snake(3, 16, [127,63,255])
-            elsif params[:command] == "middle_pulse"
-              n.middle_pulse(3, [75,40,3])
-            elsif params[:command] == "patterned_snake"
-              n.patterned_snake(3, [[127,127,127], [127,0,127], [0,127,127], [127,127,0], [127,0,0], [0,127,0], [0,0,127], [63,127,31], [31,31,0], [0,63,90], [63,63,63]])
-            elsif params[:command] == "game_of_life"
-              n.game_of_life(30, [31,127,63], [30,31,60,61,90,91,120,121,150,151,180,181])
-            end
+          if params[:command] == "on"
+            n.all_on([75,40,3])
+          elsif params[:command] == "off"
+            n.all_off
+          elsif params[:command] == "christmas"
+            n.christmas(10)
+          elsif params[:command] == "big_rainbow"
+            n.big_rainbow
+          elsif params[:command] == "dots"
+            n.moving_dots(10, 4, true, [127,127,0], [0,127,127])
+          elsif params[:command] == "lightshow"
+            n.lightshow
+          elsif params[:command] == "snake"
+            n.snake(3, 16, [127,63,255])
+          elsif params[:command] == "middle_pulse"
+            n.middle_pulse(3, [75,40,3])
+          elsif params[:command] == "patterned_snake"
+            n.patterned_snake(3, [[127,127,127], [127,0,127], [0,127,127], [127,127,0], [127,0,0], [0,127,0], [0,0,127], [63,127,31], [31,31,0], [0,63,90], [63,63,63]])
+          elsif params[:command] == "game_of_life"
+            n.game_of_life(30, [31,127,63], [30,31,60,61,90,91,120,121,150,151,180,181])
           end
-          sleep(20)
           # After we are done executing the command, broadcast a message so that the UI can unlock its buttons for the user.
           ActionCable.server.broadcast "command_channel",
             command: params[:command],
